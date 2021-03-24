fc6a509508e75872d09431d31e44539beb254d8b
DeadLordOfficial <68822716+DeadLordWasTaken@users.noreply.github.com>
2021-03-24 01:37:52 -0700

    Update binary-decode.js

--- commands/utils/binary-decode.js
+++ commands/utils/binary-decode.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-vars */
 const fetch = require('node-fetch').default;
-const Discord = require('discord.js');
 const {
 	MessageEmbed,
 } = require('discord.js');
@@ -9,10 +8,10 @@ module.exports.run = async (client, message, args) => {
 	const {
 		text,
 	} = await (await fetch(`https://some-random-api.ml/binary?decode=${encodeURI(args[0])}`)).json();
-
-	return message.channel.send(new MessageEmbed()
+	const embed = new MessageEmbed()
 		.setTitle('Here is your decoded binary!')
-		.setDescription(`**Output:**\n${text}`));
+		.setDescription(`**Output:**\n${text}`);
+	return message.reply({ embed: embed, allowedMentions: { repliedUser: false } });
 };
 
 module.exports.config = {
